06f4f2a5554c20a84e330c961f4601126b9511d9
parkjiyong1993 <jiyong5565@gmail.com>
2023-07-09 16:24:35 +0100

    Update 01-thread-and-block-idx.cu

--- CUDA/Accelerating_Applications_with_CUDA/01-thread-and-block-idx.cu
+++ CUDA/Accelerating_Applications_with_CUDA/01-thread-and-block-idx.cu
@@ -1,3 +1,8 @@
+/*Currently the 01-thread-and-block-idx.cu file contains a working kernel that is printing a failure message. 
+Open the file to learn how to update the execution configuration so that the success message will print.
+After refactoring, compile and run the code with the code execution cell below to confirm your work.
+ */
+
 #include <stdio.h>
 
 __global__ void printSuccessForCorrectExecutionConfiguration()
