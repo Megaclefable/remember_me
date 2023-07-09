8f4ecc55563622cc5aa18ee1c553c1027957bf80
parkjiyong1993 <jiyong5565@gmail.com>
2023-07-09 17:22:00 +0100

    Create 01-add-error-handling.cu

---
+++ CUDA/Accelerating_Applications_with_CUDA/01-add-error-handling.cu
@@ -0,0 +1,69 @@
+/*
+Currently 01-add-error-handling.cu compiles, runs, and prints that the elements of the array were not successfully doubled. 
+The program does not, however, indicate that there are any errors within it. 
+Refactor the application to handle CUDA errors so that you can learn what is wrong with the program and effectively debug it. 
+You will need to investigate both synchronous errors potentially created when calling CUDA functions, as well as asynchronous errors potentially created while a CUDA kernel is executing.
+Refer to the solution if you get stuck.
+*/
+
+#include <stdio.h>
+
+void init(int *a, int N)
+{
+  int i;
+  for (i = 0; i < N; ++i)
+  {
+    a[i] = i;
+  }
+}
+
+__global__
+void doubleElements(int *a, int N)
+{
+
+  int idx = blockIdx.x * blockDim.x + threadIdx.x;
+  int stride = gridDim.x * blockDim.x;
+
+  for (int i = idx; i < N + stride; i += stride)
+  {
+    a[i] *= 2;
+  }
+}
+
+bool checkElementsAreDoubled(int *a, int N)
+{
+  int i;
+  for (i = 0; i < N; ++i)
+  {
+    if (a[i] != i*2) return false;
+  }
+  return true;
+}
+
+int main()
+{
+  /*
+   * Add error handling to this source code to learn what errors
+   * exist, and then correct them. Googling error messages may be
+   * of service if actions for resolving them are not clear to you.
+   */
+
+  int N = 10000;
+  int *a;
+
+  size_t size = N * sizeof(int);
+  cudaMallocManaged(&a, size);
+
+  init(a, N);
+
+  size_t threads_per_block = 2048;
+  size_t number_of_blocks = 32;
+
+  doubleElements<<<number_of_blocks, threads_per_block>>>(a, N);
+  cudaDeviceSynchronize();
+
+  bool areDoubled = checkElementsAreDoubled(a, N);
+  printf("All elements were doubled? %s\n", areDoubled ? "TRUE" : "FALSE");
+
+  cudaFree(a);
+}
